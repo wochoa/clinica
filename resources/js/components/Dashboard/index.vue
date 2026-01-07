0bd6533069c949b3a1cd338c59eca62759da858f
wochoa <woalvarado1@gmail.com>
2026-01-07 09:48:37 -0500

    Mejorando el dashboard

--- resources/js/components/Dashboard/index.vue
+++ resources/js/components/Dashboard/index.vue
@@ -1,49 +1,101 @@
 <template>
-<div>
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">{{ $route.name }}</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <!-- <li class="breadcrumb-item">
+    <div>
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">{{ $route.name }}</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <!-- <li class="breadcrumb-item">
                             <router-link :to="'/home'">Dashboard</router-link>
                         </li> -->
-                        <li class="breadcrumb-item active">{{ $route.name }}</li>
+                            <li class="breadcrumb-item active">{{ $route.name }}</li>
 
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
 
-    <!-- Main content -->
-    <div class="content">
-        <div class="container-fluid">
-            <!-- <div class="row">
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <!-- <div class="row">
                 <div class="col-lg-6 col-10">
                     <router-link to="" class="btn btn-info btn-sm"><i class="fa-regular fa-circle-left"></i> ir a documentos en proceso</router-link>
                 </div>
 
             </div> -->
 
-            <div class="row mt-2">
-                <div class="col-4">
-                    <div class="card">
-                        este es dashboard
+                <div class="row mt-2">
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>150</h3>
+
+                                <p>Registro de citas</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa-solid fa-truck-medical"></i>
+                            </div>
+                            <router-link :to="'/citas'" class="small-box-footer">Registrar citas <i class="fas fa-arrow-circle-right"></i></router-link>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>150</h3>
+
+                                <p>Registro de pacientes</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa-solid fa-bed-pulse"></i>
+                            </div>
+                            <router-link :to="'/pacientes'" class="small-box-footer">Registrar pacientes <i class="fas fa-arrow-circle-right"></i></router-link>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>150</h3>
+
+                                <p>Registro de tratamientos</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa-solid fa-suitcase-medical"></i>
+                            </div>
+                            <router-link :to="'/tratamientos'" class="small-box-footer">Registrar tratamientos <i class="fas fa-arrow-circle-right"></i></router-link>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-primary">
+                            <div class="inner">
+                                <h3>150</h3>
+
+                                <p>Registro de dentistas</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa-solid fa-user-doctor"></i>
+                            </div>
+                            <router-link :to="'/cirujano'" class="small-box-footer">Registrar dentistas <i class="fas fa-arrow-circle-right"></i></router-link>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </div><!-- /.container-fluid -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+
     </div>
-    <!-- /.content -->
-
-</div>
 </template>
 
 <script>
@@ -66,6 +118,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
